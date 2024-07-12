2bd05d08d7ace9e803d1891e6df4790f5bf7dcc9
aizem_blaze <aizem@blaze.online>
2024-07-12 21:21:55 +0800

    Update porfolio project

--- app/components/porfolio/index.tsx
+++ app/components/porfolio/index.tsx
@@ -10,190 +10,122 @@ import {
 } from "@/components/ui/accordion";
 import ShineButton from "../magicui/shineButton";
 
+// PortfolioItem component
+const PortfolioItem = ({ image, title, techStack, link }) => (
+  <div className="p-5 shadow-md hover:shadow-xl flex flex-col items-center">
+    <Image
+      src={image.src}
+      alt={title}
+      width={image.width}
+      height={image.height}
+    />
+    <p className="text-lg mb-3">{title}</p>
+    <div className="max-w-[350px]">
+      <p>Technology Stack:</p>
+      <div className="max-w-[350px] mb-5">
+        <Accordion type="single" collapsible defaultValue="item-1">
+          {techStack.map((tech, index) => (
+            <AccordionItem key={index} value={`item-${index + 1}`}>
+              <AccordionTrigger>{tech.title}</AccordionTrigger>
+              <AccordionContent>{tech.content}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
+    </div>
+    <ShineButton>
+      <Link href={link}>Details</Link>
+    </ShineButton>
+  </div>
+);
+
+// Main Portfolio component
 const Portfolio = () => {
+  const portfolioItems = [
+    {
+      image: { src: "/wordpress.jpg", width: 350, height: 100 },
+      title: "Lovelyn Travel and Tours",
+      techStack: [
+        { title: "CMS", content: "WordPress, offering a robust and flexible foundation." },
+        { title: "Builder", content: "Elementor, utilized for designing aesthetically pleasing and responsive layouts." },
+        { title: "Booking Plugin", content: "WooCommerce Bookings, enabling efficient handling of all booking requirements directly on the website." },
+        { title: "Languages", content: "HTML, CSS, JavaScript, and PHP form the core technologies driving the dynamic and interactive elements of the site." },
+      ],
+      link: "https://lovelyntravelandtours.com/",
+    },
+    {
+      image: { src: "/cadlao.jpg", width: 350, height: 100 },
+      title: "Cadlao Travel and Tours",
+      techStack: [
+        { title: "CMS", content: "WordPress, offering a robust and flexible foundation." },
+        { title: "Builder", content: "Custom template and custom Header and footer" },
+        { title: "Languages", content: "HTML, CSS, JavaScript, and PHP form the core technologies driving the dynamic and interactive elements of the site." },
+      ],
+      link: "https://cadlaotravelandtours.com/",
+    },
+    {
+      image: { src: "/summerisland.jpg", width: 350, height: 100 },
+      title: "Summer Island Travel and Tours",
+      techStack: [
+        { title: "CMS", content: "WordPress, offering a robust and flexible foundation." },
+        { title: "Builder", content: "Custom template and custom Header and footer" },
+        { title: "Languages", content: "HTML, CSS, JavaScript, and PHP form the core technologies driving the dynamic and interactive elements of the site." },
+      ],
+      link: "https://summerislandtravelandtours.com/",
+    },
+    {
+      image: { src: "/react-project.jpg", width: 350, height: 100 },
+      title: "Evently",
+      techStack: [
+        { title: "Web Frameworks", content: "React and Next.js, used for building a dynamic and interactive user interface." },
+        { title: "JavaScript Frameworks", content: "React and Next.js, used for building a dynamic and interactive user interface." },
+        { title: "Programming Languages", content: "Node.js, supporting server-side logic and API integration." },
+        { title: "UI Frameworks", content: "Headless UI and Tailwind CSS, employed for crafting a sleek, modern, and responsive design." },
+        { title: "Platform as a Service (PaaS)", content: "Vercel, hosting the React project to leverage global distribution and effortless scaling." },
+      ],
+      link: "https://tegingl.vercel.app/",
+    },
+    {
+      image: { src: "/beachfronttravelandtours.png", width: 350, height: 100 },
+      title: "Beachfront Travel and Tours",
+      techStack: [
+        { title: "CMS", content: "WordPress, offering a robust and flexible foundation." },
+        { title: "Builder", content: "Custom template and custom Header and footer" },
+        { title: "Languages", content: "HTML, CSS, JavaScript, and PHP form the core technologies driving the dynamic and interactive elements of the site." },
+      ],
+      link: "https://beachfronttravelandtours.com/",
+    },
+        {
+      image: { src: "/kuberatravelandtours.png", width: 350, height: 100 },
+      title: "Kubera Travel and Tours",
+      techStack: [
+        { title: "CMS", content: "WordPress, offering a robust and flexible foundation." },
+        { title: "Builder", content: "Custom template and custom Header and footer" },
+        { title: "Languages", content: "HTML, CSS, JavaScript, and PHP form the core technologies driving the dynamic and interactive elements of the site." },
+      ],
+      link: "https://kuberatravelandtours.com/",
+    },
+            {
+      image: { src: "/cincotravelandtours.png", width: 350, height: 100 },
+      title: "Cinco Travel and Tours",
+      techStack: [
+        { title: "CMS", content: "WordPress, offering a robust and flexible foundation." },
+        { title: "Builder", content: "Custom template and custom Header and footer" },
+        { title: "Languages", content: "HTML, CSS, JavaScript, and PHP form the core technologies driving the dynamic and interactive elements of the site." },
+      ],
+      link: "https://cincotravelandtours.com/",
+    },
+  ];
+
   return (
-    <section className="mt-12">
+    <section className="mt-12 mb-32">
       <div className="text-5xl mb-4 mt-3 text-center">
         <h3>Portfolio Projects</h3>
       </div>
-      <div className="grid gap-5 lg:grid-cols-4 sm:grid-cols-2">
-        <div className="p-5 shadow-md hover:shadow-xl flex flex-col items-center">
-          <Image
-            src="/wordpress.jpg"
-            alt="Lovelyn Travel and Tours"
-            width={350}
-            height={100}
-          />
-          <p className="text-lg mb-3">Lovelyn Travel and Tours</p>
-          <div className="max-w-[350px]">
-            <p>Technology Stack:</p>
-            <div className="max-w-[350px] mb-5">
-              <Accordion type="single" collapsible defaultValue="item-1">
-                <AccordionItem value="item-1">
-                  <AccordionTrigger>CMS</AccordionTrigger>
-                  <AccordionContent>
-                    WordPress, offering a robust and flexible foundation.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger>Builder:</AccordionTrigger>
-                  <AccordionContent>
-                    Elementor, utilized for designing aesthetically pleasing and
-                    responsive layouts.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger>Booking Plugin:</AccordionTrigger>
-                  <AccordionContent>
-                    WooCommerce Bookings, enabling efficient handling of all
-                    booking requirements directly on the website.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-4">
-                  <AccordionTrigger>Languages:</AccordionTrigger>
-                  <AccordionContent>
-                    HTML, CSS, JavaScript, and PHP form the core technologies
-                    driving the dynamic and interactive elements of the site.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          </div>
-          <ShineButton>
-            <Link href="https://lovelyntravelandtours.com/">Details</Link>
-          </ShineButton>
-        </div>
-        <div className="p-5 shadow-md hover:shadow-xl flex flex-col items-center">
-          <Image
-            src="/cadlao.jpg"
-            alt="Cadlao Travel and Tours"
-            width={350}
-            height={100}
-          />
-          <p className="text-lg mb-3">Cadlao Travel and Tours</p>
-          <div className="max-w-[350px]">
-            <p>Technology Stack:</p>
-            <div className="max-w-[350px] mb-5">
-              <Accordion type="single" collapsible defaultValue="item-1">
-                <AccordionItem value="item-1">
-                  <AccordionTrigger>CMS</AccordionTrigger>
-                  <AccordionContent>
-                    WordPress, offering a robust and flexible foundation.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger>Builder:</AccordionTrigger>
-                  <AccordionContent>
-                    Custom template and custom Header and footer
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger>Languages:</AccordionTrigger>
-                  <AccordionContent>
-                    HTML, CSS, JavaScript, and PHP form the core technologies
-                    driving the dynamic and interactive elements of the site.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          </div>
-          <ShineButton>
-            <Link href="https://cadlaotravelandtours.com/">Details</Link>
-          </ShineButton>
-        </div>
-        <div className="p-5 shadow-md hover:shadow-xl flex flex-col items-center">
-          <Image
-            src="/summerisland.jpg"
-            alt="Summer Island Travel and Tours"
-            width={350}
-            height={100}
-          />
-          <p className="text-lg mb-3">Summer Island Travel and Tours</p>
-          <div className="max-w-[350px]">
-            <p>Technology Stack:</p>
-            <div className="max-w-[350px] mb-5">
-              <Accordion type="single" collapsible defaultValue="item-1">
-                <AccordionItem value="item-1">
-                  <AccordionTrigger>CMS</AccordionTrigger>
-                  <AccordionContent>
-                    WordPress, offering a robust and flexible foundation.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger>Builder:</AccordionTrigger>
-                  <AccordionContent>
-                    Custom template and custom Header and footer
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger>Languages:</AccordionTrigger>
-                  <AccordionContent>
-                    HTML, CSS, JavaScript, and PHP form the core technologies
-                    driving the dynamic and interactive elements of the site.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          </div>
-          <ShineButton>
-            <Link href="https://summerislandtravelandtours.com/">Details</Link>
-          </ShineButton>
-        </div>
-        <div className="p-5 shadow-md hover:shadow-xl flex flex-col items-center">
-          <Image
-            src="/react-project.jpg"
-            alt="Evently"
-            width={350}
-            height={100}
-          />
-          <p className="text-lg mb-3">Evently</p>
-          <div className="max-w-[350px]">
-            <p>Technology Stack:</p>
-            <div className="max-w-[350px] mb-5">
-              <Accordion type="single" collapsible defaultValue="item-1">
-                <AccordionItem value="item-1">
-                  <AccordionTrigger>Web Frameworks: Next.js</AccordionTrigger>
-                  <AccordionContent>
-                    React and Next.js, used for building a dynamic and
-                    interactive user interface.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-2">
-                  <AccordionTrigger>JavaScript Frameworks:</AccordionTrigger>
-                  <AccordionContent>
-                    React and Next.js, used for building a dynamic and
-                    interactive user interface.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-3">
-                  <AccordionTrigger>Programming Languages:</AccordionTrigger>
-                  <AccordionContent>
-                    Node.js, supporting server-side logic and API integration.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-4">
-                  <AccordionTrigger>UI Frameworks:</AccordionTrigger>
-                  <AccordionContent>
-                    Headless UI and Tailwind CSS, employed for crafting a sleek,
-                    modern, and responsive design.
-                  </AccordionContent>
-                </AccordionItem>
-                <AccordionItem value="item-5">
-                  <AccordionTrigger>
-                    Platform as a Service (PaaS):
-                  </AccordionTrigger>
-                  <AccordionContent>
-                    Vercel, hosting the React project to leverage global
-                    distribution and effortless scaling.
-                  </AccordionContent>
-                </AccordionItem>
-              </Accordion>
-            </div>
-          </div>
-          <ShineButton>
-            <Link href="https://tegingl.vercel.app/">Details</Link>
-          </ShineButton>
-        </div>
+      <div className="grid gap-5 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+        {portfolioItems.map((item, index) => (
+          <PortfolioItem key={index} {...item} />
+        ))}
       </div>
     </section>
   );
